04f7004b7dca32e6f3b5f018f7270a83867b2be9
Taufikurrahman Anwar <16517087@office.itb.ac.id>
2019-03-19 19:15:43 +0700

    Dragon

--- DesktopApp2/Form1.Designer.cs
+++ DesktopApp2/Form1.Designer.cs
@@ -28,38 +28,34 @@ namespace DesktopApp2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            Microsoft.Msagl.Core.Geometry.Curves.PlaneTransformation planeTransformation2 = new Microsoft.Msagl.Core.Geometry.Curves.PlaneTransformation();
+            this.askbutton = new System.Windows.Forms.Button();
             this.Browse = new System.Windows.Forms.Button();
             this.FileDir = new System.Windows.Forms.TextBox();
             this.FerdiantStart = new System.Windows.Forms.ComboBox();
             this.JoseAddress = new System.Windows.Forms.ComboBox();
             this.Direction = new System.Windows.Forms.ComboBox();
+            this.gViewer1 = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+            this.InputDir = new System.Windows.Forms.TextBox();
+            this.Input = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // button1
+            // askbutton
             // 
-            this.button1.Location = new System.Drawing.Point(445, 40);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(76, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Ask!";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.DefaultExt = "txt";
-            this.openFileDialog1.FileName = "openFileDialog1";
-            this.openFileDialog1.InitialDirectory = "C:\\";
-            this.openFileDialog1.Title = "Browse Text FIle";
-            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            this.askbutton.Location = new System.Drawing.Point(561, 111);
+            this.askbutton.Margin = new System.Windows.Forms.Padding(2);
+            this.askbutton.Name = "askbutton";
+            this.askbutton.Size = new System.Drawing.Size(76, 23);
+            this.askbutton.TabIndex = 2;
+            this.askbutton.Text = "Ask!";
+            this.askbutton.UseVisualStyleBackColor = true;
+            this.askbutton.Click += new System.EventHandler(this.button1_Click);
             // 
             // Browse
             // 
-            this.Browse.Location = new System.Drawing.Point(446, 12);
+            this.Browse.Location = new System.Drawing.Point(562, 41);
             this.Browse.Name = "Browse";
             this.Browse.Size = new System.Drawing.Size(75, 23);
             this.Browse.TabIndex = 4;
@@ -69,25 +65,26 @@ namespace DesktopApp2
             // 
             // FileDir
             // 
-            this.FileDir.Location = new System.Drawing.Point(194, 12);
+            this.FileDir.Location = new System.Drawing.Point(343, 41);
             this.FileDir.Name = "FileDir";
-            this.FileDir.Size = new System.Drawing.Size(231, 20);
+            this.FileDir.Size = new System.Drawing.Size(200, 20);
             this.FileDir.TabIndex = 5;
             // 
             // FerdiantStart
             // 
             this.FerdiantStart.FormattingEnabled = true;
-            this.FerdiantStart.Location = new System.Drawing.Point(358, 42);
+            this.FerdiantStart.Location = new System.Drawing.Point(483, 113);
             this.FerdiantStart.Name = "FerdiantStart";
-            this.FerdiantStart.Size = new System.Drawing.Size(67, 21);
+            this.FerdiantStart.Size = new System.Drawing.Size(60, 21);
             this.FerdiantStart.TabIndex = 6;
+            this.FerdiantStart.SelectedIndexChanged += new System.EventHandler(this.FerdiantStart_SelectedIndexChanged);
             // 
             // JoseAddress
             // 
             this.JoseAddress.FormattingEnabled = true;
-            this.JoseAddress.Location = new System.Drawing.Point(276, 42);
+            this.JoseAddress.Location = new System.Drawing.Point(413, 113);
             this.JoseAddress.Name = "JoseAddress";
-            this.JoseAddress.Size = new System.Drawing.Size(67, 21);
+            this.JoseAddress.Size = new System.Drawing.Size(60, 21);
             this.JoseAddress.TabIndex = 7;
             // 
             // Direction
@@ -97,39 +94,120 @@ namespace DesktopApp2
             this.Direction.Items.AddRange(new object[] {
             "1",
             "0"});
-            this.Direction.Location = new System.Drawing.Point(194, 42);
+            this.Direction.Location = new System.Drawing.Point(343, 113);
             this.Direction.Name = "Direction";
-            this.Direction.Size = new System.Drawing.Size(67, 21);
+            this.Direction.Size = new System.Drawing.Size(60, 21);
             this.Direction.TabIndex = 8;
             this.Direction.SelectedIndexChanged += new System.EventHandler(this.Direction_SelectedIndexChanged);
+            // 
+            // gViewer1
+            // 
+            this.gViewer1.ArrowheadLength = 10D;
+            this.gViewer1.AsyncLayout = false;
+            this.gViewer1.AutoScroll = true;
+            this.gViewer1.BackwardEnabled = false;
+            this.gViewer1.BuildHitTree = true;
+            this.gViewer1.CurrentLayoutMethod = Microsoft.Msagl.GraphViewerGdi.LayoutMethod.UseSettingsOfTheGraph;
+            this.gViewer1.EdgeInsertButtonVisible = true;
+            this.gViewer1.FileName = "";
+            this.gViewer1.ForwardEnabled = false;
+            this.gViewer1.Graph = null;
+            this.gViewer1.InsertingEdge = false;
+            this.gViewer1.LayoutAlgorithmSettingsButtonVisible = true;
+            this.gViewer1.LayoutEditingEnabled = true;
+            this.gViewer1.Location = new System.Drawing.Point(12, 12);
+            this.gViewer1.LooseOffsetForRouting = 0.25D;
+            this.gViewer1.MouseHitDistance = 0.05D;
+            this.gViewer1.Name = "gViewer1";
+            this.gViewer1.NavigationVisible = true;
+            this.gViewer1.NeedToCalculateLayout = true;
+            this.gViewer1.OffsetForRelaxingInRouting = 0.6D;
+            this.gViewer1.PaddingForEdgeRouting = 8D;
+            this.gViewer1.PanButtonPressed = false;
+            this.gViewer1.SaveAsImageEnabled = true;
+            this.gViewer1.SaveAsMsaglEnabled = true;
+            this.gViewer1.SaveButtonVisible = true;
+            this.gViewer1.SaveGraphButtonVisible = true;
+            this.gViewer1.SaveInVectorFormatEnabled = true;
+            this.gViewer1.Size = new System.Drawing.Size(291, 334);
+            this.gViewer1.TabIndex = 9;
+            this.gViewer1.TightOffsetForRouting = 0.125D;
+            this.gViewer1.ToolBarIsVisible = true;
+            this.gViewer1.Transform = planeTransformation2;
+            this.gViewer1.UndoRedoButtonsVisible = true;
+            this.gViewer1.WindowZoomButtonPressed = false;
+            this.gViewer1.ZoomF = 1D;
+            this.gViewer1.ZoomWindowThreshold = 0.05D;
+            // 
+            // InputDir
+            // 
+            this.InputDir.Location = new System.Drawing.Point(344, 141);
+            this.InputDir.Name = "InputDir";
+            this.InputDir.Size = new System.Drawing.Size(200, 20);
+            this.InputDir.TabIndex = 10;
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(562, 139);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(75, 23);
+            this.Input.TabIndex = 11;
+            this.Input.Text = "Browse";
+            this.Input.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(341, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Question";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(344, 173);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(293, 173);
+            this.listBox1.TabIndex = 13;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.ClientSize = new System.Drawing.Size(650, 358);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.InputDir);
+            this.Controls.Add(this.gViewer1);
             this.Controls.Add(this.Direction);
             this.Controls.Add(this.JoseAddress);
             this.Controls.Add(this.FerdiantStart);
             this.Controls.Add(this.FileDir);
             this.Controls.Add(this.Browse);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.askbutton);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Negeri Antah-Berantah";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button askbutton;
         private System.Windows.Forms.Button Browse;
         private System.Windows.Forms.TextBox FileDir;
         private System.Windows.Forms.ComboBox FerdiantStart;
         private System.Windows.Forms.ComboBox JoseAddress;
         private System.Windows.Forms.ComboBox Direction;
+        private Microsoft.Msagl.GraphViewerGdi.GViewer gViewer1;
+        private System.Windows.Forms.TextBox InputDir;
+        private System.Windows.Forms.Button Input;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- DesktopApp2/Form1.cs
+++ DesktopApp2/Form1.cs
@@ -8,6 +8,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Msagl.Drawing;
+using Microsoft.Msagl.Core;
+using Microsoft.Msagl.GraphViewerGdi;
 
 // This is the code for your desktop app.
 // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
@@ -16,28 +19,25 @@ namespace DesktopApp2
 {
     public partial class Form1 : Form
     {
-        private Graph graf;
-        private int[] node;
+        Graph graph = new Graph("grapf");
+        GrafMatriks grafMatriks;
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            // Click on the link below to continue learning how to build a desktop app using WinForms!
-            System.Diagnostics.Process.Start("http://aka.ms/dotnet-get-started-desktop");
-
-        }
-
+        
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Thanks!");
+            // grafMatriks.printGraf();
+            int dir = Convert.ToInt32(Direction.Text);
+            int jose = Convert.ToInt32(JoseAddress.Text) - 1;
+            int ferd = Convert.ToInt32(FerdiantStart.Text) - 1;
+            grafMatriks.solve(dir, jose, ferd);
         }
 
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
-
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -66,32 +66,31 @@ namespace DesktopApp2
             string filename = openFileDialog1.FileName;
             string[] filelines = File.ReadAllLines(filename);
             // Inisialisasi Graph
-            graf = new Graph(Convert.ToInt32(filelines[0].Trim()));
-            node = new int[Convert.ToInt32(filelines[0].Trim())];
-            initNode(node, Convert.ToInt32(filelines[0].Trim()));
-            JoseAddress.DataSource = node;
-            FerdiantStart.DataSource = node;
+            grafMatriks = new GrafMatriks(Convert.ToInt32(filelines[0].Trim()));
+            initComboBox(Convert.ToInt32(filelines[0].Trim()));
             for (int i = 1; i < filelines.Length; i++)
             {
-                string[] spltmp = filelines[i].Split(' ');
-                int row = Convert.ToInt32(spltmp[1]) - 1;
-                int col = Convert.ToInt32(spltmp[0]) - 1;
-                if (row < col)
-                {
-                    row = row + col;
-                    col = row - col;
-                    row = row - col;
-                }
+                string a, b;
+                int row, col;
+                string[] splt;
+                splt = filelines[i].Split(' ');
+                a = splt[0];
+                b = splt[1];
+                row = Convert.ToInt32(a) - 1;
+                col = Convert.ToInt32(b) - 1;
+                graph.AddEdge(a, b);
                 Console.WriteLine(Convert.ToString(row) + ' ' + Convert.ToString(col));
-                graf.setGraf(row, col);
+                grafMatriks.setGraf(row,col,true);
             }
+            gViewer1.Graph = graph;
         }
-        
-        private void initNode(int[] node, int size)
+
+        private void initComboBox(int size)
         {
-            for(int i = 1; i <= size; i++)
+            for (int i = 1; i <= size; i++)
             {
-                node[i - 1] = i;
+                JoseAddress.Items.Add(i);
+                FerdiantStart.Items.Add(i);
             }
         }
 
@@ -99,19 +98,172 @@ namespace DesktopApp2
         {
 
         }
-    }
 
-    public class Graph
+        private void FerdiantStart_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class GrafMatriks
     {
         private bool[][] graph;
         private int size;
-
-        public bool solve(int direction)
+        
+        public void solve(int dir, int jose, int ferd)
         {
-            return true;
+            Console.Write(jose); Console.Write(' '); Console.WriteLine(ferd);
+            if (dir == 1)
+            {
+                Console.WriteLine(Convert.ToString(solveAway(jose, ferd)));
+            }
+            else
+            {
+                Console.WriteLine(Convert.ToString(solveTowards(jose, ferd)));
+            }
+        }
+        
+        public bool solveTowards(int jose, int ferd)
+        {
+            if (ferd == jose)
+            {
+                return true;
+            }
+            else
+            {
+                if (ferd != 1)
+                {
+                    bool flag = false;
+                    for (int i = 0; i < size; i++)
+                    {
+                        if (neighbor(ferd, i))
+                        {
+                            if (canReachThrough(ferd, i, 0))
+                            {
+                                setGraf(ferd, i, false);
+                                flag = solveTowards(jose, i);
+                                setGraf(ferd, i, true);
+                                if (flag)
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    Console.WriteLine("Tidak Bisa");
+                    return flag;
+                }
+                else
+                {
+                    Console.WriteLine("Tidak Bisa");
+                    return false;
+                }
+            }
         }
 
-        public Graph(int s)
+        public bool solveAway(int jose, int ferd)
+        {
+            if (ferd == jose)
+            {
+                Console.WriteLine("Bisa");
+                return true;
+            }
+            else
+            {
+                if (haveNeighbor(ferd))
+                {
+                    bool flag = false;
+                    for (int i = 0; i < size; i++)
+                    {
+                        if (neighbor(ferd, i))
+                        {
+                            if (!canReachThrough(ferd, i, 0))
+                            {
+                                setGraf(ferd, i, false);
+                                flag = solveAway(jose, i);
+                                setGraf(ferd, i, true);
+                                if (flag){
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    Console.WriteLine("Tidak Bisa");
+                    return flag;
+                }
+                else
+                {
+                    Console.WriteLine("Tidak Bisa");
+                    return false;
+                }
+            }
+        }
+        
+        public bool canReachThrough(int node1, int node2, int target)
+        {
+            Console.Write(node1); Console.Write(" tes "); Console.Write(node2); Console.WriteLine();
+            if (haveNeighbor(node2))
+            {
+                Console.Write("Have Neighbor");
+                Console.WriteLine();
+                if (neighbor(node2, target))
+                {
+                    return true;
+                }
+                else
+                {
+                    bool flag = false;
+                    for(int i = 0; i < size; i++)
+                    {
+                        Console.Write(node2); Console.Write(' '); Console.Write(i); Console.Write(' '); Console.Write(target);
+                        Console.WriteLine();
+                        if (neighbor(node2, i) && (node1 != i))
+                        {
+                            if (canReachThrough(node2, i, target))
+                            {
+                                flag = true;
+                            }
+                        }
+                    }
+                    return flag;
+                }
+            }
+            else
+            {
+                Console.Write("Don't Have Neighbor");
+                return false;
+            }
+        }
+
+        public bool haveNeighbor(int node)
+        {
+            int i = 0;
+            while ((i < size) && !neighbor(node, i))
+            {
+                i++;
+            }
+            return (i < size);
+        }
+
+        public bool neighbor(int node1, int node2)
+        {
+            if (node1 < node2)
+            {
+                return graph[node2][node1];
+            }
+            else
+            {
+                if (node1 == node2)
+                {
+                    return false;
+                }
+                else
+                {
+                    return graph[node1][node2];
+                }
+            }
+        }
+
+        public GrafMatriks(int s)
         {
             size = s;
             graph = new bool[size][];
@@ -126,9 +278,29 @@ namespace DesktopApp2
             return size;
         }
 
-        public void setGraf(int row, int col)
+        public void setGraf(int row, int col, bool val)
         {
-            graph[row][col] = true;
+            if (row < col)
+            {
+                graph[col][row] = val;
+            }
+            else
+            {
+                graph[row][col] = val;
+            }
+        }
+
+        public void printGraf()
+        {
+            for(int i = 0; i < size; i++)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    Console.Write(neighbor(j,i));
+                    Console.Write(' ');
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
